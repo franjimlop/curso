25312b3fe0da20bf8ec07b9db7b221d8e8fac543
franjimlop <franciscojl2000@gmail.com>
2023-02-07 21:49:53 +0100

    Creación de servicio REST

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,12 +1,50 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value="prueba")
-    public String prueba() {
-        return "prueba";
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Fran");
+        usuario.setApellido("Jiménez");
+        usuario.setEmail("alumno.401733@ies-azarquiel.es");
+        usuario.setTelefono("666900238");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario2")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Fran");
+        usuario.setApellido("Jiménez");
+        usuario.setEmail("alumno.401733@ies-azarquiel.es");
+        usuario.setTelefono("666900238");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario3")
+    public Usuario eliminar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Fran");
+        usuario.setApellido("Jiménez");
+        usuario.setEmail("alumno.401733@ies-azarquiel.es");
+        usuario.setTelefono("666900238");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario4")
+    public Usuario buscar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Fran");
+        usuario.setApellido("Jiménez");
+        usuario.setEmail("alumno.401733@ies-azarquiel.es");
+        usuario.setTelefono("666900238");
+        return usuario;
     }
 }
