06d43795b78a4039298cd0fb39eb0f181bd4810a
franjimlop <franciscojl2000@gmail.com>
2023-02-12 20:19:39 +0100

    daos creados

---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -0,0 +1,12 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+
+import java.util.List;
+
+public class UsuarioDaoImp implements UsuarioDao {
+    @Override
+    public List<Usuario> getUsuarios() {
+        return null;
+    }
+}
