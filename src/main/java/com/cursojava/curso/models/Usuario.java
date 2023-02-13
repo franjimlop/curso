ded56a89a770ea0b2ab967697b1ad086cde5d750
franjimlop <franciscojl2000@gmail.com>
2023-02-13 22:52:35 +0100

    Agregar usuarios

--- src/main/java/com/cursojava/curso/models/Usuario.java
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -5,16 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "usuarios")
 @Entity
 @ToString @EqualsAndHashCode
 public class Usuario {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter @Column(name = "id")
     private Long id;
     @Getter @Setter @Column(name = "nombre")
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -17,9 +17,10 @@ async function cargaUsuario() {
     let listadoHTML = '';
     for (let usuario of usuarios) {
         let botonEliminar = '<a href="#" onclick = "eliminarUsuario ('+ usuario.id +')" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i></a>'
+        let telefono = usuario.telefono == null ? '-': usuario.telefono;
 
         let usuarioHTML = '<tr><td>' + usuario.id + '</td><td>' + usuario.nombre + ' ' + usuario.apellido + '</td><td>'
-        + usuario.email + '</td><td>' + usuario.telefono + '</td><td>' + botonEliminar +'</td></tr>'
+        + usuario.email + '</td><td>' + telefono + '</td><td>' + botonEliminar +'</td></tr>'
         listadoHTML += usuarioHTML;
     }
     document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
