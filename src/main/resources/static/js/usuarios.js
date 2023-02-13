3236bf168b7503559d78372513a2998679b56bb5
franjimlop <franciscojl2000@gmail.com>
2023-02-13 20:29:24 +0100

    Eliminar usuarios

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -16,9 +16,27 @@ async function cargaUsuario() {
 
     let listadoHTML = '';
     for (let usuario of usuarios) {
-        let usuarioHTML = '<tr><td>' + usuario.id + '</td><td>' + usuario.nombre + ' ' + usuario.apellido + '</td><td>' + usuario.email + '</td><td>'
-            + usuario.telefono + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i></a></td></tr>'
+        let botonEliminar = '<a href="#" onclick = "eliminarUsuario ('+ usuario.id +')" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i></a>'
+
+        let usuarioHTML = '<tr><td>' + usuario.id + '</td><td>' + usuario.nombre + ' ' + usuario.apellido + '</td><td>'
+        + usuario.email + '</td><td>' + usuario.telefono + '</td><td>' + botonEliminar +'</td></tr>'
         listadoHTML += usuarioHTML;
     }
     document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
+}
+
+async function eliminarUsuario(id) {
+    if (!confirm('¿Desea eliminar este usuario?')){
+        return;
+    }
+
+    const request = await fetch('api/usuarios/' + id, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Context-Type': 'application/json'
+            }
+    });
+
+    location.reload()
 }
